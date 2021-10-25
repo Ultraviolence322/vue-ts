d8324c45c02ba113cc5053bc7f17c224489d20f8
Ultraviolence322 <segeichenkov18@gmail.com>
2021-10-25 17:44:14 +0300

    added prop of term in JobList

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="job-list">
+    <p class="mt-8 mb-4">
+      Order by - <span class="font-bold">{{term}}</span>
+    </p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
-        <h2>{{job.title}}</h2>
-        <p class="job-list__location">{{job.location}}</p>
-        <p class="job-list__salary">{{job.salary}}</p>
+      <li 
+        v-for="job in sortedJobs" :key="job.id"
+        class="p-2 border-4 border-blue-500 mb-4 rounded"
+      >
+        <h2 class="text-2xl font-bold pb-2"><span class="text-sm font-normal">Title: </span>{{job.title}}</h2>
+        <p class="job-list__location"><span class="text-sm">Location: </span> {{job.location}}</p>
+        <p class="job-list__salary text-green-500"><span class="text-sm text-black">Salary:</span> ${{job.salary}}</p>
       </li>
     </ul>
   </div>
@@ -12,18 +18,32 @@
 
 <script lang="ts">
 import Job from '@/types/Job'
-import { defineComponent, PropType } from 'vue'
+import OrderTerm from '@/types/OrderTerm'
+import { computed, defineComponent, PropType } from 'vue'
 
 export default defineComponent({
   props: {
     jobs: {
-      require: true,
+      required: true,
       type: Array as PropType<Job[]>
+    },
+    term: {
+      required: true,
+      type: String as PropType<OrderTerm>
     }
   },
 
-  setup() {
-    
+  setup(props) {
+    const sortedJobs = computed(()=> {
+      return props.jobs.sort((a: Job, b: Job) => {
+        return a[props.term] < b[props.term] ? 1 : -1
+      })
+
+    })
+
+    return {
+      sortedJobs
+    }
   },
 })
 </script>
