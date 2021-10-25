7f420df04bb8df5a6efa15835c47720a342eef83
Ultraviolence322 <segeichenkov18@gmail.com>
2021-10-25 17:43:49 +0300

    added OrderTerms components

---
+++ src/components/OrderTerms.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="order-terms">
+    <button @click="selectTerm('salary')" class="p-2 border-2 rounded border-green-500 mr-4 hover:bg-green-500 hover:text-white">Order by salary</button>
+    <button @click="selectTerm('title')" class="p-2 border-2 rounded border-green-500 mr-4 hover:bg-green-500 hover:text-white">Order by title</button>
+    <button @click="selectTerm('location')" class="p-2 border-2 rounded border-green-500 hover:bg-green-500 hover:text-white">Order by location</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import OrderTerm from '@/types/OrderTerm'
+
+export default defineComponent({
+  setup(props, context) {
+    const term = ref<OrderTerm>('salary')
+
+    const selectTerm = (selectedTerm: OrderTerm) => {
+      term.value = selectedTerm
+
+      context.emit('selectedTerm', selectedTerm)
+    }
+
+    return {
+      term,
+      selectTerm
+    }
+  },
+})
+</script>
